4fa64993b200288270f2c71e4200d0641be6168b
Bogdan Bruma <bbruma51@yahoo.com>
2024-10-28 18:01:43 +0100

    Updated and configured flags

--- IDS.py
+++ IDS.py
@@ -1,36 +1,49 @@
+####Block spiaddress when malicious traffic is detected
+####Use proper os subprograms
+import argparse
 from scapy.all import sniff, send, IP, TCP, UDP,wrpcap
 import os
 import pandas as pd
 import time
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-import joblib
 import pickle
+import subprocess
+import logging
 
-with open('trained_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+# Function to check the timer
+def check_timer():
+    elapsed_time = time.time() - start_time
 
-# Loading the label encoder
-with open('label_encoder.pkl', 'rb') as encoder_file:
-    loaded_label_encoder = pickle.load(encoder_file)
+    return elapsed_time
 
-# Function to classify packets
+# Function to reset the timer
+def reset_timer():
+    global start_time
+    start_time = time.time()
 
 
-# Function to handle the packet processing
+
+
+
+# Function to either append or anallyze the packets depending on the specification of the window
 def packet_callback(packet):
     
     if IP in packet:
             packet_buffer.append(packet)
-    print("Packet: "+str(len(packet_buffer)))
-    if (len(packet_buffer) == 10000):
+
+    if (len(packet_buffer) == MaxPackets or check_timer() >= TimeWindows):
+ 
         wrpcap('buffer.pcap', packet_buffer)
         packet_buffer.clear()
         packetsToFlow()
 
+# Function to convert packets to flows
 def packetsToFlow():
-    os.system("yaf -i buffer.pcap -o flows.flw")
-    time.sleep(2)
-    os.system("yafscii -i flows.flw -o flows.csv --tabular")
+
+    subprocess.run(["yaf -i buffer.pcap -o flows.flw"],shell=True)
+    time.sleep(1)
+
+    subprocess.run(["yafscii -i flows.flw -o flows.csv --tabular"],shell=True)
     file_name = 'flows.csv'
     header = 'start-time             |end-time               |duration|rtt     |proto|sip                                   |sp   |dip                                     |dp   |iflags  |uflags  |riflags |ruflags |isn     |risn    |tag|rtag|pkt    |oct     |rpkt    |roct    |end-reason\n'
 
@@ -46,9 +59,10 @@ def packetsToFlow():
     with open(file_name, 'w') as file:
         file.write(header)         # Write the header
         file.write(original_contents)  # Write the original conten
-    print("Flow file created and headers added!")
+
     extractFlowFeatures()
 
+# Function to extract features from the flows
 def extractFlowFeatures():
     df = pd.read_csv ('flows.csv', sep='|')
     df.columns = df.columns.str.strip()
@@ -57,32 +71,66 @@ def extractFlowFeatures():
     df['start-time'] = pd.to_datetime(df['start-time'])
     df['end-time'] = pd.to_datetime(df['end-time'])
     le = LabelEncoder()
-    #df['proto'] = le.fit_transform(df['proto'])
-    #df['tag'] = le.fit_transform(df['tag'])
-    #df['rtag'] = le.fit_transform(df['rtag'])
-    #df['sp'] = le.fit_transform(df['sp'])
     X = df.drop(columns=['start-time','end-time', 'sip','dip','dp','iflags','uflags','ruflags','riflags','isn','risn','end-reason'])
     classifyFlow(X)
 
+# Function to classify the flows
 def classifyFlow(X):
-    counter = 0
     y_pred = model.predict(X)
-    #print(X.head())
-    print(y_pred)
-    #print("Flow classified!")
+
     for i in range(len(y_pred)):
         if y_pred[i] == True:
-            counter += 1
-        else:
-           print(X.iloc[i])
-    #print("Flow classification done!")
-    print("Number of malicious flows: "+str(counter))
+            logging.warning(X.iloc[i])
+            
+        
+
+    reset_timer()
     exit()
 
 
 
-# Sniff all incoming traffic
-print("Sniffing incoming traffic...")
+
+
+
+parser = argparse.ArgumentParser(description="A simple AI based IDS.")
+parser.add_argument("-t", "--min_time", type=int, help="Minimum number of seconds to sniff traffic for(default 20). Time is relative to the first packet recieved for the window .May run for less if MAX PACKETS is reached")
+parser.add_argument("-m", "--max_packets", type=int, help="Maximum number of packets to sniff(default 10000)")
+args = parser.parse_args()
+
+with open('trained_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# Loading the label encoder
+with open('label_encoder.pkl', 'rb') as encoder_file:
+    loaded_label_encoder = pickle.load(encoder_file)
+
+
+
+
+
+
+logging.basicConfig(
+    filename="ids.log",  # Log file name
+    filemode="a",        # Append to the file (use 'w' to overwrite)
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO    # Log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+)
+
+
 packet_buffer = []
+start_time = time.time()
+if args.min_time:
+    TimeWindows=args.min_time
+else:
+    TimeWindows=20
+
+if args.max_packets:
+    MaxPackets=args.max_packets 
+else:
+    MaxPackets=10000
+
+print("Configuration:")
+print("Time window: "+str(TimeWindows))
+print("Max packets: "+str(MaxPackets))
 sniff(iface='eth0', prn=packet_callback, store=0)  # Specify the correct interface
 # sniff(offline = 'capture.pcap', prn=packet_callback, store=0)
