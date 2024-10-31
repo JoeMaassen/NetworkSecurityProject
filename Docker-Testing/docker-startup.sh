b9fa8228875400dc81cc77e6e31a2e5dc6afa5d3
NONGENTESIMUS <nhkjbdwkhjbchkhwh@gmail.com>
2024-10-31 14:39:44 +0100

    Attacker gets additional attack

--- Docker-Testing/docker-startup.sh
+++ Docker-Testing/docker-startup.sh
@@ -7,11 +7,14 @@ cp ../IDS/IDS.py IDS.py
 cp ../PreTrainedModels/trained_model_srf.pkl trained_model.pkl # Here change the path to get a diffrent model
 cp ../PreTrainedModels/label_encoder.pkl label_encoder.pkl
 cp ../Data/AttackSimulationDataSet.pcap AttackSimulationDataSet.pcap
+cp ../Data/smallerAttackSimulation.pcap smallerAttackSimulation.pcap
+
 docker compose up --build -d
 rm IDS.py
 rm trained_model.pkl
 rm label_encoder.pkl
 rm AttackSimulationDataSet.pcap
+rm smallerAttackSimulation.pcap
 
 gnome-terminal -- bash -c "sh ./Docker-Testing/target.sh; exec bash"
 gnome-terminal -- bash -c "sh ./Docker-Testing/attacker.sh; exec bash"
