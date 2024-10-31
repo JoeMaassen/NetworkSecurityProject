6e502f2dff690274980765f33d937e324ed70ccb
NONGENTESIMUS <nhkjbdwkhjbchkhwh@gmail.com>
2024-10-31 16:48:03 +0100

    fixed error

--- Docker-Testing/attacker.sh
+++ Docker-Testing/attacker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker exec -it attacker /bin/bash -c '
 read -p "Press any key to continue... " -n1 -s
-tcpreplay -i eth0 --mbps=60 -l 1000 smallerAttackSimulation.pcap.pcap
+tcpreplay -i eth0 --mbps=60 -l 1000 smallerAttackSimulation.pcap
 '
